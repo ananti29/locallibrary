1c2f8e5fcf47236b00d9e96f1edda062ac43be28
ananti29 <antin29@hotmail.com>
2018-10-04 20:42:51 +0300

    part 2 challenge yourself

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
+router.get('/cool', function (req, res, next) {
+    res.send('I´m so cool. I know how to route!');
+});
+
 module.exports = router;
