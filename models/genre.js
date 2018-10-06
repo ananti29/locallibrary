edc5b0d40e92ebf38f454ec767441a40594c3d6d
ananti29 <antin29@hotmail.com>
2018-10-06 20:14:25 +0300

    Genre schema

---
+++ models/genre.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+    {
+        kirja: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+        tyylilajin_nimi: { type: String, required: true, min: 3, max: 100 }
+    }
+);
+
+// Virtual for bookinstance's URL
+GenreSchema
+    .virtual('url')
+    .get(function virtualGenreUrl () {
+        return '/catalog/genre/' + this._id;
+    });
+
+// Export model
+module.exports = mongoose.model('BookInstance', GenreSchema);
