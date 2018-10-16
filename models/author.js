5d72aaef450acff242d44eaab46a6356d8999020
ananti29 <antin29@hotmail.com>
2018-10-17 01:30:33 +0300

    updated npm packages and fixed virtual names in author

--- models/author.js
+++ models/author.js
@@ -22,7 +22,7 @@ AuthorSchema
 
 // Virtual for author's age
 AuthorSchema
-    .virtual('age')
+    .virtual('ika')
     .get(function virtualAge () {
         let born = this.syntyma_aika ? moment(this.syntyma_aika).locale('fi').format('YYYY') : '';
         let death = this.kuolin_aika ? moment(this.kuolin_aika).locale('fi').format('YYYY') : '';
@@ -53,7 +53,7 @@ AuthorSchema
 
 // Virtual for kuolin and syntyma_aika together in more user friendly mode
 AuthorSchema
-    .virtual('age_day_format')
+    .virtual('ika_paiva_format')
     .get(function ikapaivaformat () {
         let deathday = this.kuolin_aika ? moment(this.kuolin_aika).locale('fi').format('D MMMM YYYY') : '';
         let bornday = this.syntyma_aika ? moment(this.syntyma_aika).locale('fi').format('D MMMM YYYY') : '';
